284b5235bd0daf1745161ed43eb14a89108f8f9d
Hadi Faraz <hadifaraz52@protonmail.com>
2024-01-04 13:07:00 -0700

    Remove unused imports

--- src/main/java/org/hadifaraz/server/Main.java
+++ src/main/java/org/hadifaraz/server/Main.java
@@ -1,6 +1,9 @@
 package org.hadifaraz.server;
 
+import java.io.*;
+
 public class Main {
+
     public static void main(String[] args) {
         try {
             Server server = new Server("jdbc:sqlite:db/chats.db", 60430);
