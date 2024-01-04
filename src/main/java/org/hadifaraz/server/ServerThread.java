217c76c34a2cb726a80d606f9fd8d8faa24e2bf8
Hadi Faraz <hadifaraz52@protonmail.com>
2024-01-04 13:09:26 -0700

    Add start function call
    Add promptErr call to run function

--- src/main/java/org/hadifaraz/server/ServerThread.java
+++ src/main/java/org/hadifaraz/server/ServerThread.java
@@ -13,21 +13,29 @@ public class ServerThread extends Thread {
     public ServerThread(String userName, Server server, Socket socket) throws IOException {
         this.server = server;
         this.socket = socket;
-        this.dos = new DataOutputStream(socket.getOutputStream());
-        this.dis = new DataInputStream(socket.getInputStream());
         this.userName = userName;
+
+        start();
     }
 
     public void run() {
         try {
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+            //noinspection InfiniteLoopStatement
             while (true) {
                 String message = dis.readUTF();
+                System.out.println("Received message.");
 
                 server.sendMsg(userName, message);
+                System.out.println("Sent message to all clients.");
             }
+        } catch (EOFException ignored) {
         } catch (IOException e) {
-            System.out.println("Do something");
-        } finally {
+            System.out.println("IOException has occurred.");
+
+            Misc.promptErr(e);
+        }  finally {
             server.removeConnection(userName);
         }
     }
