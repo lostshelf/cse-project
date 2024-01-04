8616c2f007eb14688dae14a31debf6a4cd23af8e
Hadi Faraz <hadifaraz52@protonmail.com>
2024-01-04 13:05:33 -0700

    Add more verbose logging
    Replace ActionListener construction with a lamdba function

--- src/main/java/org/hadifaraz/client/Client.java
+++ src/main/java/org/hadifaraz/client/Client.java
@@ -1,5 +1,7 @@
 package org.hadifaraz.client;
 
+import org.hadifaraz.server.Misc;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
@@ -9,55 +11,54 @@ import javax.swing.*;
 
 
 public class Client extends Panel implements Runnable {
-    private TextField tf = new TextField();
-    private TextArea ta = new TextArea();
+    private final TextField tf = new TextField();
+    private final TextArea ta = new TextArea();
     private Socket socket;
     private DataOutputStream dos;
     private DataInputStream dis;
     public Client(String host, int port) {
-
         setLayout(new BorderLayout());
 
-        add("North", tf);
+        add("South", tf);
         add("Center", ta);
 
-        tf.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                processMsg(e.getActionCommand());
-            }
-        });
+        tf.addActionListener(e -> processMsg(e.getActionCommand()));
 
         try {
-            System.out.println("Connecting to server.");
             socket = new Socket(host, port);
+            System.out.println("Connected to server.");
 
             dis = new DataInputStream(socket.getInputStream());
             dos = new DataOutputStream(socket.getOutputStream());
 
-            new Thread(this).start();
+            System.out.println("Data streams created.");
 
+            new Thread(this).start();
+            System.out.println("Thread started.");
         } catch (IOException ignored) {}
     }
 
     public void processMsg(String msg) {
-        System.out.println("Got a message.");
+        System.out.printf("Received message \"%s\" from client.%n", msg);
+
         try {
-            System.out.println("Sending to server.");
             dos.writeUTF(msg);
+            System.out.println("Sent to server.");
 
             tf.setText("");
-        } catch (IOException ignored) {}
+        } catch (IOException e) {
+            Misc.promptErr(e);
+        }
     }
 
     public void run() {
         try {
             while (true) {
-                System.out.println("Waiting for message from server.");
                 String msg = dis.readUTF();
+                System.out.println("Received message from server.");
 
-                System.out.println("Printing message");
                 ta.append(String.format("%s%n", msg));
+                System.out.println("Displaying message.");
             }
         } catch (IOException ignored) {}
     }
