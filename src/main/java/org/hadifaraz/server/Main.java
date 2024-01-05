609b6f0834d62a06d6ad16638cfd908fcf69cbc5
Hadi Faraz <hadifaraz52@protonmail.com>
2024-01-04 20:42:02 -0700

    Remove the sendMsg

--- src/main/java/org/hadifaraz/server/Main.java
+++ src/main/java/org/hadifaraz/server/Main.java
@@ -7,8 +7,6 @@ public class Main {
     public static void main(String[] args) {
         try {
             Server server = new Server("jdbc:sqlite:db/chats.db", 60430);
-
-            server.sendMsg("Server", "bals");
         } catch (Exception ignored) {}
     }
 }
