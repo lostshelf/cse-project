24da83629cbe5033ef2bbd7ddee40d21061807c0
lostshelf <75292074+lostshelf@users.noreply.github.com>
2024-01-12 11:16:06 -0700

    Update Main.java

--- src/main/java/org/hadifaraz/server/Main.java
+++ src/main/java/org/hadifaraz/server/Main.java
@@ -2,6 +2,7 @@ package org.hadifaraz.server;
 
 public class Main {
     public static void main(String[] args) {
+        // User must provide the port to run on via CLI args
         if (args.length == 0) {
             System.out.println("Usage: Server <PORT>");
 
@@ -9,6 +10,8 @@ public class Main {
         }
 
         try {
+            // Create a new server
+            // Will need to change the first argument depending on where the database is and what the SQLite driver is
             Server server = new Server("jdbc:sqlite:db/chats.db", Integer.parseInt(args[0]));
         } catch (Exception ignored) {}
     }
