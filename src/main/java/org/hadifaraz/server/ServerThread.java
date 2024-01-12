624b7f9f62d30ec72b189ace1325ba5fe4df6d34
lostshelf <75292074+lostshelf@users.noreply.github.com>
2024-01-12 11:26:40 -0700

    Update ServerThread.java

--- src/main/java/org/hadifaraz/server/ServerThread.java
+++ src/main/java/org/hadifaraz/server/ServerThread.java
@@ -3,6 +3,7 @@ package org.hadifaraz.server;
 import java.io.*;
 import java.net.*;
 
+// Handles recieving messages from the Clients
 public class ServerThread extends Thread {
     Server server;
     DataInputStream dis;
@@ -15,6 +16,8 @@ public class ServerThread extends Thread {
         this.socket = socket;
         this.userName = userName;
 
+        // Make sure the Thread actually starts
+        // Spent WAY too much time trying to figure out why the Server wasn't recieving messages and it was all because I forgot to run this function
         start();
     }
 
@@ -22,11 +25,14 @@ public class ServerThread extends Thread {
         try {
             DataInputStream dis = new DataInputStream(socket.getInputStream());
 
+            // Stop IntelliJ from whining about it
             //noinspection InfiniteLoopStatement
             while (true) {
+                // Read message from the client
                 String message = dis.readUTF();
                 System.out.println("Received message.");
 
+                // Send it to all the other clients
                 server.sendMsg(userName, message);
                 System.out.println("Sent message to all clients.");
             }
@@ -36,6 +42,7 @@ public class ServerThread extends Thread {
 
             Misc.promptErr(e);
         }  finally {
+            // Have to remove the connection or else the program will be wasting resources 
             server.removeConnection(userName);
         }
     }
