bcdbab25bc6939f1b607abecdf1a482babd92633
lostshelf <75292074+lostshelf@users.noreply.github.com>
2024-01-12 11:17:29 -0700

    Update Misc.java

--- src/main/java/org/hadifaraz/server/Misc.java
+++ src/main/java/org/hadifaraz/server/Misc.java
@@ -3,11 +3,13 @@ package org.hadifaraz.server;
 import java.util.*;
 
 public class Misc {
+    // Prompts the user if they want to see the error
     public static void promptErr(Exception e) {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Would you like to see the error? (y, n): ");
 
+        // Will fail if nothing is input
         char input = scan.nextLine().trim().toLowerCase().charAt(0);
 
         if (input == 'n')
